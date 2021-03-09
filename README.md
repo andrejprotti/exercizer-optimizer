ea3006dbb9f5feaf8c1a0649176e15e91ab4cd55
André J. Protti <andrejardimprotti@gmail.com>
2021-03-09 20:54:28 +0100

    Refactoring the methods in smaller pieces!
    Also updating the README again!

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # 💪💪💪 Optimize your workouts! 💪💪💪
 
+_requirements: ruby 3.0.0 and rspec 3.10.0_
+
+Running the sample and the tests, while in the directory:
+```sh
+ruby exercizer_optimizer.rb
+rspec exerciser_spec.rb
+```
+
 Some important allies that helped me with this but don't show up in the imports: rubocop and pry!
 
 Cool upgrades I perceive:
-- Factory Girl to make the tests cleaner
+- Factory Girl to make the test scenario inputs cleaner
 - Docker+Docker-Compose (could really help with local testing and running if it were to become a micro-service in the future)
-- Validating inputs and more extensive stress testing (maybe with chaos monkey?)
+- Validating (and testing) inputs
+- Stress testing
 - As much optimization as possible regarding cyclomatic complexity
 - Prepare it for an API style microservice (inputs and outputs in JSON)! Could be cool to show it on the front-end, specially if there's some sort of GraphQL already around
+- Define the inputs clearly with everyone to create a model for saving in DBs!
--- exerciser.rb
+++ exerciser.rb
@@ -1,40 +1,10 @@
 # frozen_string_literal: true
 
-def add_to_workout(id, current_time, exercise, possible_solutions)
-  return current_time if (current_time <= 0) || (exercise[:average_span] > current_time)
-
-  possible_solutions[id].push(exercise)
-  current_time - exercise[:average_span]
-end
-
 def maximize_workout(exercises_hash)
   total_workout_time = exercises_hash[:time]
   available_exercises = exercises_hash[:exercises]
-
-  exercise_ids = []
-  possible_combinations = []
-  possible_solutions = {}
-
-  available_exercises.each do |lead_exercise|
-    exercise_ids.push(lead_exercise[:id])
-  end
-
-  (1..available_exercises.length).each do |counter|
-    possible_combinations.push(exercise_ids.combination(counter).to_a)
-  end
-
-  possible_combinations.each do |combination|
-    combination.each do |solution|
-      current_time = total_workout_time
-      solution_key = solution.to_s
-      possible_solutions[solution_key] = []
-      solution.to_a.each do |exercise_id|
-        exercise = available_exercises.select { |exs| exs[:id] == exercise_id }
-        current_time = add_to_workout(solution_key, current_time, exercise[0], possible_solutions)
-      end
-    end
-  end
-
+  possible_combinations = generate_combinations(available_exercises)
+  possible_solutions = calculate_possible_solutions(possible_combinations, total_workout_time, available_exercises)
   max_calories = 0
   best_workout = []
 
@@ -52,4 +22,44 @@ def maximize_workout(exercises_hash)
   end
 
   { max_calories: max_calories, exercises: best_workout }
+end
+
+def add_to_workout(id, current_time, exercise, possible_solutions)
+  return current_time if (current_time <= 0) || (exercise[:average_span] > current_time)
+
+  possible_solutions[id].push(exercise)
+  current_time - exercise[:average_span]
+end
+
+def generate_combinations(exercises)
+  possible_combinations = []
+  exercise_ids = []
+
+  exercises.each do |exercise|
+    exercise_ids.push(exercise[:id])
+  end
+
+  (1..exercises.length).each do |counter|
+    possible_combinations.push(exercise_ids.combination(counter).to_a)
+  end
+
+  possible_combinations
+end
+
+def calculate_possible_solutions(possible_combinations, total_workout_time, available_exercises)
+  possible_solutions = {}
+
+  possible_combinations.each do |combination|
+    combination.each do |solution|
+      current_time = total_workout_time
+      solution_key = solution.to_s
+      possible_solutions[solution_key] = []
+      solution.to_a.each do |exercise_id|
+        exercise = available_exercises.select { |exs| exs[:id] == exercise_id }
+        current_time = add_to_workout(solution_key, current_time, exercise[0], possible_solutions)
+      end
+    end
+  end
+
+  possible_solutions
 end
